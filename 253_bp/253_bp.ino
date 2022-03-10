3ad4b4e6be79418dea32248cbf5105e1730cf848
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-03-10 14:39:17 +0530

    Add files via upload

---
+++ 253_bp/253_bp.ino
@@ -0,0 +1,44 @@
+//  including ezbutton module (easy button module)
+#include<ezButton.h>
+
+//  connect both buttons to pin 4 and 22
+const int button1 = 4;
+const int button2 = 22;
+
+//  defining a variable to track count of button press
+int count = 0;
+
+//  creating button objects
+ezButton push_button1(button1);
+ezButton push_button2(button2);
+
+void setup()
+{
+  //  change the baud rate to 9600 if required
+  Serial.begin(115200);
+  push_button1.setDebounceTime(50);  //  debounce time = 50 ms
+  push_button2.setDebounceTime(50);
+}
+
+void loop()
+{
+  //  loop both the push_buttons : continuous polling
+  
+
+
+  //  check if push_button1 is pressed : store the button push count into count variable
+  if (push_button1.isPressed())
+  {
+    
+  }
+
+  //  else if push_button2 is pressed : print the table of the number stored in variable count
+  else if (push_button2.isPressed())
+  {
+    //  draft a loop to print the table
+
+
+    //  reset the number to 0, once table is printed
+  }
+ 
+}
